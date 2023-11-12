30efa2225098cfa2a477a1850c96a68deada0f2e
laffra <laffra@gmail.com>
2023-11-12 18:03:24 +0100

    Add hello world example

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,10 +1,12 @@
 # LTK - Copyrights Reserved 2023 - chrislaffra.com - See LICENSE 
 
 from examples import custom
+from examples import helloworld
 from examples import table
 from examples import tictactoe
 
 elements = [
+    helloworld.create(),
     tictactoe.create(),
     table.create(),
     custom.create(),
---
+++ examples/helloworld.py
@@ -0,0 +1,8 @@
+# LTK - Copyrights Reserved 2023 - chrislaffra.com - See LICENSE 
+
+import ltk
+
+hello = ltk.Text("Hello World")
+
+def create():
+    return hello.attr("name", "Hello World")
