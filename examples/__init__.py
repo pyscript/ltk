850f7ffda127c7dbf6c4abb38e36f7bc2a7597c3
laffra <laffra@gmail.com>
2023-11-21 13:56:17 +0100

    Add PyData information

--- examples/__init__.py
+++ examples/__init__.py
@@ -5,10 +5,12 @@ from examples import custom
 from examples import documentation
 from examples import helloworld
 from examples import inputs
+from examples import pydata
 from examples import table
 from examples import tictactoe
 
 items = [
+    ("examples/pydata.py", pydata.create()),
     ("examples/helloworld.py", helloworld.create()),
     ("examples/inputs.py", inputs.create()),
     ("examples/tictactoe.py", tictactoe.create()),
---
+++ examples/pydata.py
@@ -0,0 +1,38 @@
+# LTK - Copyrights Reserved 2023 - chrislaffra.com - See LICENSE 
+
+import ltk
+
+def create():
+    return (
+        ltk.VBox(
+
+            ltk.Image("https://pydata.org/wp-content/uploads/2019/06/pydata-logo-final.png")
+                .css("margin-top", 50)
+                .css("width", 350),
+
+            ltk.Link(
+                "https://pydata.org/eindhoven2023/",
+                "Come visit PyData Eindhoven"
+            )
+                .attr("target", "_blank")
+                .css("color", "navy")
+                .css("margin-top", 50),
+
+            ltk.Text("on November 30 and learn more about LTK, the Pyscript Little Toolkit")
+                .css("margin-top", 50),
+
+            ltk.Link(
+                "https://eindhoven2023.pydata.org/pydata/talk/RJVRMT/",
+                "in this presentation"
+            )
+                .attr("target", "_blank")
+                .css("color", "navy")
+                .css("margin-top", 50),
+
+            ltk.Text("by Chris Laffra")
+                .css("margin-top", 50),
+
+        )
+        .css("font-size", 32)
+        .attr("name", "Pydata")
+    )
