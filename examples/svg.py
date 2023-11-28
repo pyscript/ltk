8b84c8eadcfbb36ac199bc93a361dd4617fc2eb6
laffra <laffra@gmail.com>
2023-11-29 00:07:40 +0100

    Add explanation to SVG example

--- examples/svg.py
+++ examples/svg.py
@@ -10,6 +10,10 @@ def create():
     def bind_event():
         ltk.find("#ellipse").on("click", lambda event: ellipse_clicked())
 
+    #
+    # Only in this specific example we need to bind events after
+    # the svg is actually added to the DOM, so we use ltk.schedule
+    #
     ltk.schedule(bind_event)
 
     return(
@@ -19,7 +23,8 @@ def create():
                 <svg id="svg" height="180" width="500">
                     <ellipse id="ellipse" cx="200" cy="80" rx="100" ry="50" style="fill:yellow;stroke:purple;stroke-width:2" />
                 </svg>
-            """)
+            """),
+            ltk.Heading3("Click the yellow ellipse to change the SVG's background color."),
         )
         .attr("name", "SVG")
     )
