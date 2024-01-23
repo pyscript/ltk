094e10b46ea05661e0edefd418909228430fdc66
laffra <laffra@gmail.com>
2024-01-23 14:36:21 +0100

    Add debug statements to pubsub

--- ltk/pubsub.py
+++ ltk/pubsub.py
@@ -77,15 +77,20 @@ class _PubSub():
     def process_queue(self):
         for key, message in list(self.queue.items()):
             del self.queue[key] # remove the message from the queue
+            print("pubsub: remove from queue:", key)
             handled = False
             for subscriber in self.subscribers:
                 if self.match(message, *subscriber):
+                    print("pubsub: matched:", key)
                     handled = True
             if not handled:
+                print("pubsub: put back on queue:", key)
                 self.queue[key] = message # put back the message onto the queue
 
     def add_to_queue(self, message):
-        self.queue[f"{_name}-{time.time()}"] = message
+        key = f"{_name}-{time.time()}"
+        print("pubsub: add to queue:", key)
+        self.queue[key] = message
 
     def publish(self, sender, receiver, topic, data):
         self.add_to_queue(_Message(sender, receiver, topic, data))
